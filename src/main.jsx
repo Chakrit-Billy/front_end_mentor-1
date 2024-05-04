0678b9901a09e1eacb92ac04cdd495221def5b73
Billy <abl.chakrit@gmail.com>
2024-05-04 11:10:10 +0700

    template

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Mainpage from "./mainPage.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Mainpage />
+  </React.StrictMode>
+);
